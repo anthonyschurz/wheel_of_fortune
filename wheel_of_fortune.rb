de741ee0310db087790df056fa3128fc873e42d1
anthonyschurz <anthony.schurz@gmail.com>
2016-06-14 08:50:03 -0700

    makes changes

--- wheel_of_fortune.rb
+++ wheel_of_fortune.rb
@@ -1,31 +1,46 @@
 class WheelOfFortune
-  def initialize
-    p "Happy coding!"
+  attr_reader :theme, :guesses
+
+  def initialize(params)
+    @theme = params[:theme] || ""
+    @phrase = params[:phrase] || ""
+    @remaining_characters = unique_chars
+    @guesses = []
   end
 
   def to_s
-    nil
+    return @phrase if game_over?
+    @phrase.gsub(unguessed, "_")
   end
 
   def can_i_have?(input)
-    nil
+    char = sanitize_guess(input)
+    good_guess?(char)
   end
 
   def game_over?
-    nil
+    @remaining_characters.empty?
   end
-end
 
+  private
 
-# Checks to see if we executed this file from the command-line
-# e.g. `ruby simple_jepordy.rb`
-if __FILE__ == $0
+  def sanitize_guess(input)
+    input.strip.downcase
+  end
 
-  # DRIVER CODE
-  j = WheelOfFortune.new( {theme: "card games", phrase: "Go fish"} )
-  j.can_i_have?("g")
-  j.can_i_have?("o")
-  p j
-  puts j # calls j.to_s
+  def add_guess(guess)
+    @guesses << guess unless guess.empty?
+  end
 
+  def good_guess?(char)
+    !!(add_guess(char) && @remaining_characters.delete(char))
+  end
+
+  def unguessed
+    Regexp.new(@remaining_characters.join('|'), Regexp::IGNORECASE)
+  end
+
+  def unique_chars
+    @phrase.downcase.delete("^a-z").split("").uniq
+  end
 end
